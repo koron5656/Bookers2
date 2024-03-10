f23520181f8f750695a45a9132ab8e7562852f47
EC2 Default User <ec2-user@ip-172-31-21-88.ap-northeast-1.compute.internal>
2024-03-10 09:52:35 +0000

    [Finish]update、destroyページ機能作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,7 +8,7 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
     @user = @book.user
   end
-  
+
   def edit
     @book = Book.find(params[:id])
   end
@@ -23,6 +23,23 @@ class BooksController < ApplicationController
     @book.user_id = current_user.id
     @book.save
     redirect_to book_path(@book)
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :edit
+    end
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to "/books"
   end
 
   private
