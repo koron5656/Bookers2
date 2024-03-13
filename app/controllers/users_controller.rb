0d18c78a553052f44766f4e17ad42751906e63fa
EC2 Default User <ec2-user@ip-172-31-21-88.ap-northeast-1.compute.internal>
2024-03-13 07:58:11 +0000

    [Finish]UserUpdateのバリデーション

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @book = Book.all
+    @book = @user.books
   end
 
   def edit
@@ -15,15 +15,19 @@ class UsersController < ApplicationController
 
   def update
     @user = User.find(params[:id])
-    @user.update(user_params)
-    flash[:notice] = "プロフィール更新に成功しました。"
-    redirect_to user_path(@user.id)
+    if @user.update(user_params)
+      flash[:notice] = "プロフィール更新に成功しました。"
+      redirect_to user_path(@user.id)
+    else
+      @book = @user.books
+      render:edit
+    end
   end
 
 private
 
   def user_params
-    params.require(:user).permit(:name, :profile_image)
+    params.require(:user).permit(:name, :profile_image, :introduction)
   end
 
   def is_matching_login_user
